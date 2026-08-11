f826bb63f19121be7bd34eb07d2be7f71ae9b9a1
yaoxianglongdeguge <2162582869@qq.com>
2026-08-11 14:32:45 +0800

    每次处理一个请求后，都把后面数据拷到前面来，虽然有更多开销，但是逻辑上简化，不容易出bug，而且更加适配一些函数，比如memcpy拷贝，这种的如果要拷一段连续但是折返的数据就很麻烦

--- http_analysis/http_ed_store.c
+++ http_analysis/http_ed_store.c
@@ -4,8 +4,8 @@ typedef struct http_ed_store{
 
     char* begin;//指向整体最开始
     char* ptr_b;//指向顺序来看，有效位置的最开始
-    char* ptr_e;//指向顺序来看，有效位置末端.
-    char* end;//指向整体末端
+    char* ptr_e;//指向顺序来看，有效位置末端后一位.
+    char* end;//指向整体末端后一位
 
 }http_ed_store;//这个就是http解析前存储的地方，需要解决的问题是，如果我们一次读固定长度数据，那就有可能除了读到所要的请求之外还读到下一个请求的一部分，而如果没有标记，
 //那我们下次继续读可能读到的是上上次请求残留的无效位，而如果直接再接收从头读，那就有可能这次请求的上一半段还没读到
@@ -22,8 +22,39 @@ http_ed_store* Http_ed_store_init(http_ed_store* h,int size)//分配整个存储
     h->ptr_e=h->begin;
 
     return h;
-    int a=0;
 
+}
+
+int Http_ed_store_accept(http_ed_store* h,int fd)
+{
+   
+        int length=h->end-h->ptr_e;
+        int n1=read(fd,h->ptr_e,length);
+        if(n1==1)
+        {
+         h->ptr_e=h->end;
+        }
+        else
+        {
+            return -1;
+        }
+
+    return 1;
+}
+
+int Http_ed_store_copy(http_ed_store* h,char*end,char* target)//把一整个请求拷走，然后把后面的数据拷到开头
+{
+    int copy_num=end-h->begin+1;
+    memcpy(target, h->begin, copy_num);
+
+    h->ptr_b=h->begin+copy_num;
+
+    int copy_num2=h->ptr_e-h->ptr_b;
+    memcpy(h->begin,h->ptr_b,copy_num2);
+    h->ptr_b=h->begin;
+    h->ptr_e=h->ptr_b+copy_num2;
+
+    return 1;
 }
 
 
--- http_analysis/http_ed_store.h
+++ http_analysis/http_ed_store.h
@@ -1,1 +1,9 @@
 #include<stdio.h>
+
+
+http_ed_store* Http_ed_store_init(http_ed_store* h,int size);//分配整个存储大小
+
+int Http_ed_store_accept(http_ed_store* h,int fd);//从连接中读数据填满无效位
+
+int Http_ed_store_copy(http_ed_store* h,char*end,char* target);//从有效位开始到要拷贝数据末端(包含末端)把数据拷贝到指定位置(包含该位置)，如果要拷贝的范围大于有效范围则返回2
+//end是要拷贝的末端，target是拷贝后存储位置
