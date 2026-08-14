be4b0c6cbe5aa17c092ba9deb26cac459c62fd9c
yaoxianglongdeguge <2162582869@qq.com>
2026-08-14 21:21:59 +0800

    读写锁写好

--- my_lock/my_rwlock_t.c
+++ my_lock/my_rwlock_t.c
@@ -1,1 +1,52 @@
 #include "my_rwlock_t.h"
+#include<pthread.h>
+
+int my_rwlock_init(my_rwlock_t* rw)
+{
+    pthread_mutex_init(&(rw->mutex), NULL);
+    pthread_cond_init(&(rw->cond), NULL);
+    rw->readers = 0;
+    rw->writers = 0;
+    rw->waiting_writers = 0;
+
+    return 1;
+}
+
+int my_lock_rdlock(my_rwlock_t* rw)
+{
+    pthread_mutex_lock(&(rw->mutex));
+    while(rw->waiting_writers!=0||rw->waiting_writers!=0)
+    {
+        pthread_cond_wait(&(rw->cond),&(rw->mutex));
+    }
+    rw->readers++;
+    pthread_mutex_unlock(&(rw->mutex));
+}
+
+int my_lock_wrlock(my_rwlock_t* rw)
+{
+    pthread_mutex_lock(&(rw->mutex));
+    rw->waiting_writers++;
+    while(rw->readers>0||rw->writers>0)
+    {
+        pthread_cond_wait(&(rw->cond),&(rw->mutex));
+    }
+    rw->waiting_writers--;
+    rw->writers=1;
+    pthread_mutex_unlock(&(rw->mutex));
+}
+
+int my_lock_unlock(my_rwlock_t* rw)
+{
+    pthread_mutex_lock(&(rw->mutex));
+    if(rw->readers>0)
+    {
+        rw->readers--;
+    }
+    else if(rw->writers=1)
+    {
+        rw->writers=0;
+    }
+    pthread_cond_broadcast(&rw->cond);
+    pthread_mutex_unlock(&(rw->mutex));
+}
